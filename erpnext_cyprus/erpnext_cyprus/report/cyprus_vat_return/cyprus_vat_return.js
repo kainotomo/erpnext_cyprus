01dc61c0de27606b17f4561a38c05d0bf5fde802
Panayiotis Halouvas <phalouvas@gmail.com>
2025-04-23 07:42:37 +0000

    Add Cyprus VAT Return report implementation with filters and data retrieval

---
+++ erpnext_cyprus/erpnext_cyprus/report/cyprus_vat_return/cyprus_vat_return.js
@@ -0,0 +1,163 @@
+frappe.query_reports["Cyprus VAT Return"] = {
+    "filters": [
+        {
+            "fieldname": "company",
+            "label": __("Company"),
+            "fieldtype": "Link",
+            "options": "Company",
+            "default": frappe.defaults.get_user_default("Company"),
+            "reqd": 1
+        },
+        {
+            "fieldname": "fiscal_year",
+            "label": __("Fiscal Year"),
+            "fieldtype": "Link",
+            "options": "Fiscal Year",
+            "reqd": 1,
+            "on_change": function() {
+                updateDateRange();
+            }
+        },
+        {
+            "fieldname": "quarter",
+            "label": __("Quarter"),
+            "fieldtype": "Select",
+            "options": [
+                "Q1 (Jan-Mar)",
+                "Q2 (Apr-Jun)",
+                "Q3 (Jul-Sep)",
+                "Q4 (Oct-Dec)"
+            ],
+            "default": getCurrentQuarter(),
+            "reqd": 1,
+            "on_change": function() {
+                updateDateRange();
+            }
+        },
+        {
+            "fieldname": "from_date",
+            "label": __("From Date"),
+            "fieldtype": "Date",
+            "hidden": 1
+        },
+        {
+            "fieldname": "to_date",
+            "label": __("To Date"),
+            "fieldtype": "Date",
+            "hidden": 1
+        }
+    ],
+    "formatter": function(value, row, column, data, default_formatter) {
+        value = default_formatter(value, row, column, data);
+        if (column.fieldname == "amount" && data && data.bold) {
+            value = "<b>" + value + "</b>";
+        }
+        return value;
+    },
+    "onload": function(report) {
+        // Find and set the current fiscal year
+        setCurrentFiscalYear();
+        
+        report.page.add_inner_button(__("Download Cyprus VAT Format"), function() {
+            // This would generate the official VAT format download
+            let filters = report.get_values();
+            
+            frappe.call({
+                method: "erpnext_cyprus.erpnext_cyprus.report.cyprus_vat_return.cyprus_vat_return.export_vat_return",
+                args: {
+                    filters: filters
+                },
+                callback: function(r) {
+                    if (r.message) {
+                        window.open(r.message);
+                    }
+                }
+            });
+        });
+    }
+};
+
+function getCurrentQuarter() {
+    const currentMonth = new Date().getMonth() + 1; // JavaScript months are 0-indexed
+    
+    if (currentMonth >= 1 && currentMonth <= 3) return "Q1 (Jan-Mar)";
+    if (currentMonth >= 4 && currentMonth <= 6) return "Q2 (Apr-Jun)";
+    if (currentMonth >= 7 && currentMonth <= 9) return "Q3 (Jul-Sep)";
+    return "Q4 (Oct-Dec)";
+}
+
+function setCurrentFiscalYear() {
+    const today = frappe.datetime.get_today();
+    
+    // Find the fiscal year containing today's date
+    frappe.db.get_list('Fiscal Year', {
+        fields: ['name'],
+        filters: {
+            'disabled': 0
+        }
+    }).then(years => {
+        const promises = years.map(year => {
+            return new Promise(resolve => {
+                frappe.db.get_value('Fiscal Year', year.name, ['year_start_date', 'year_end_date'])
+                    .then(value => {
+                        resolve({
+                            name: year.name,
+                            start: value.message.year_start_date,
+                            end: value.message.year_end_date
+                        });
+                    });
+            });
+        });
+        
+        Promise.all(promises).then(fiscalYears => {
+            const currentFY = fiscalYears.find(fy => {
+                return today >= fy.start && today <= fy.end;
+            });
+            
+            if (currentFY) {
+                frappe.query_report.set_filter_value('fiscal_year', currentFY.name);
+                updateDateRange();
+            }
+        });
+    });
+}
+
+function updateDateRange() {
+    let quarter = frappe.query_report.get_filter_value('quarter');
+    let fiscal_year = frappe.query_report.get_filter_value('fiscal_year');
+    
+    if (!quarter || !fiscal_year) return;
+    
+    // Get the year part from fiscal year
+    frappe.db.get_value("Fiscal Year", fiscal_year, ["year_start_date", "year_end_date"], function(value) {
+        if (!value) return;
+        
+        let year = new Date(value.message.year_start_date).getFullYear();
+        let from_date, to_date;
+        
+        // Extract quarter value from the display string
+        let q = quarter.substring(0, 2);
+        
+        switch(q) {
+            case "Q1":
+                from_date = year + "-01-01";
+                to_date = year + "-03-31";
+                break;
+            case "Q2":
+                from_date = year + "-04-01";
+                to_date = year + "-06-30";
+                break;
+            case "Q3":
+                from_date = year + "-07-01";
+                to_date = year + "-09-30";
+                break;
+            case "Q4":
+                from_date = year + "-10-01";
+                to_date = year + "-12-31";
+                break;
+        }
+        
+        frappe.query_report.set_filter_value('from_date', from_date);
+        frappe.query_report.set_filter_value('to_date', to_date);
+    });
+}
---
+++ erpnext_cyprus/erpnext_cyprus/report/cyprus_vat_return/cyprus_vat_return.py
@@ -0,0 +1,441 @@
+# Copyright (c) 2023, Your Company and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe import _
+from frappe.utils import flt, getdate, get_first_day, get_last_day, add_days, add_months
+
+def execute(filters=None):
+    # Ensure from_date and to_date are set
+    if not filters.get('from_date') or not filters.get('to_date'):
+        fiscal_year = filters.get('fiscal_year')
+        quarter = filters.get('quarter')
+        if fiscal_year and quarter:
+            dates = get_quarter_dates(fiscal_year, quarter)
+            filters['from_date'] = dates['from_date']
+            filters['to_date'] = dates['to_date']
+    
+    return get_columns(), get_data(filters)
+
+def get_quarter_dates(fiscal_year, quarter):
+    """Get the start and end dates for the specified quarter"""
+    if not fiscal_year or not quarter:
+        return {'from_date': None, 'to_date': None}
+    
+    # Get fiscal year start date
+    year_details = frappe.get_doc("Fiscal Year", fiscal_year)
+    year = getdate(year_details.year_start_date).year
+    
+    # Extract quarter info
+    quarter_start = None
+    quarter_end = None
+    
+    if "Q1" in quarter:
+        quarter_start = f"{year}-01-01"
+        quarter_end = f"{year}-03-31"
+    elif "Q2" in quarter:
+        quarter_start = f"{year}-04-01"
+        quarter_end = f"{year}-06-30"
+    elif "Q3" in quarter:
+        quarter_start = f"{year}-07-01"
+        quarter_end = f"{year}-09-30"
+    elif "Q4" in quarter:
+        quarter_start = f"{year}-10-01"
+        quarter_end = f"{year}-12-31"
+    
+    return {
+        'from_date': quarter_start,
+        'to_date': quarter_end
+    }
+
+def get_columns():
+    return [
+        {
+            "fieldname": "vat_field",
+            "label": _("Field"),
+            "fieldtype": "Data",
+            "width": 120
+        },
+        {
+            "fieldname": "description",
+            "label": _("Description"),
+            "fieldtype": "Data",
+            "width": 300
+        },
+        {
+            "fieldname": "amount",
+            "label": _("Amount (EUR)"),
+            "fieldtype": "Currency",
+            "options": "currency",
+            "width": 150
+        }
+    ]
+
+def get_data(filters):
+    company = filters.get("company")
+    from_date = filters.get("from_date")
+    to_date = filters.get("to_date")
+    
+    if not company or not from_date or not to_date:
+        return []
+    
+    # Initialize VAT return data
+    vat_return_data = []
+    
+    # Add VAT Return fields according to Cyprus tax requirements
+    # Box 1: VAT due on sales and other outputs
+    output_vat = get_output_vat(company, from_date, to_date)
+    vat_return_data.append({
+        "vat_field": _("Box 1"),
+        "description": _("VAT due on sales and other outputs"),
+        "amount": output_vat
+    })
+
+    # Box 2: VAT due on acquisitions from EU countries
+    eu_acquisitions_vat = get_eu_acquisitions_vat(company, from_date, to_date)
+    vat_return_data.append({
+        "vat_field": _("Box 2"),
+        "description": _("VAT due on acquisitions from EU countries"),
+        "amount": eu_acquisitions_vat
+    })
+    
+    # Box 3: Total VAT due (box 1 + box 2)
+    total_vat_due = flt(output_vat) + flt(eu_acquisitions_vat)
+    vat_return_data.append({
+        "vat_field": _("Box 3"),
+        "description": _("Total VAT due (sum of boxes 1 and 2)"),
+        "amount": total_vat_due,
+        "bold": 1
+    })
+    
+    # Box 4: VAT reclaimed on purchases and other inputs
+    input_vat = get_input_vat(company, from_date, to_date)
+    vat_return_data.append({
+        "vat_field": _("Box 4"),
+        "description": _("VAT reclaimed on purchases and other inputs"),
+        "amount": input_vat
+    })
+    
+    # Box 5: Net VAT to be paid or reclaimed
+    net_vat = flt(total_vat_due) - flt(input_vat)
+    vat_return_data.append({
+        "vat_field": _("Box 5"),
+        "description": _("Net VAT to be paid or reclaimed"),
+        "amount": net_vat,
+        "bold": 1
+    })
+    
+    # Box 6: Total value of sales and other outputs excluding VAT
+    total_sales = get_total_sales(company, from_date, to_date)
+    vat_return_data.append({
+        "vat_field": _("Box 6"),
+        "description": _("Total value of sales and other outputs excluding VAT"),
+        "amount": total_sales
+    })
+    
+    # Box 7: Total value of purchases and inputs excluding VAT
+    total_purchases = get_total_purchases(company, from_date, to_date)
+    vat_return_data.append({
+        "vat_field": _("Box 7"),
+        "description": _("Total value of purchases and inputs excluding VAT"),
+        "amount": total_purchases
+    })
+    
+    # Box 8A & 8B: EU goods and services supplies
+    eu_supplies_goods = get_eu_goods_supplies(company, from_date, to_date)
+    eu_supplies_services = get_eu_services_supplies(company, from_date, to_date)
+    vat_return_data.append({
+        "vat_field": _("Box 8A"),
+        "description": _("Total value of supplies of goods to EU countries"),
+        "amount": eu_supplies_goods
+    })
+    vat_return_data.append({
+        "vat_field": _("Box 8B"),
+        "description": _("Total value of supplies of services to EU countries"),
+        "amount": eu_supplies_services
+    })
+    
+    # Box 9: Total value of exports to non-EU countries
+    non_eu_exports = get_non_eu_exports(company, from_date, to_date)
+    vat_return_data.append({
+        "vat_field": _("Box 9"),
+        "description": _("Total value of exports to non-EU countries"),
+        "amount": non_eu_exports
+    })
+    
+    # Box 10: Total value of out-of-scope sales
+    out_of_scope = get_out_of_scope_sales(company, from_date, to_date)
+    vat_return_data.append({
+        "vat_field": _("Box 10"),
+        "description": _("Total value of out-of-scope sales"),
+        "amount": out_of_scope
+    })
+    
+    # Box 11A & 11B: EU goods and services acquisitions
+    eu_acquisitions_goods = get_eu_goods_acquisitions(company, from_date, to_date)
+    eu_acquisitions_services = get_eu_services_acquisitions(company, from_date, to_date)
+    vat_return_data.append({
+        "vat_field": _("Box 11A"),
+        "description": _("Total value of acquisitions of goods from EU countries"),
+        "amount": eu_acquisitions_goods
+    })
+    vat_return_data.append({
+        "vat_field": _("Box 11B"),
+        "description": _("Total value of acquisitions of services from EU countries"),
+        "amount": eu_acquisitions_services
+    })
+    
+    return vat_return_data
+
+def get_output_vat(company, from_date, to_date):
+    # Get VAT collected on sales
+    output_vat = frappe.db.sql("""
+        SELECT SUM(credit - debit) as vat_amount
+        FROM `tabGL Entry`
+        WHERE posting_date BETWEEN %s AND %s
+        AND company = %s
+        AND account IN (
+            SELECT name FROM `tabAccount`
+            WHERE account_type = 'Tax' AND account_name LIKE '%%Output%%'
+            AND company = %s
+        )
+    """, (from_date, to_date, company, company), as_dict=1)
+    
+    return flt(output_vat[0].vat_amount) if output_vat and output_vat[0].vat_amount is not None else 0
+
+def get_input_vat(company, from_date, to_date):
+    # Get VAT paid on purchases
+    input_vat = frappe.db.sql("""
+        SELECT SUM(debit - credit) as vat_amount
+        FROM `tabGL Entry`
+        WHERE posting_date BETWEEN %s AND %s
+        AND company = %s
+        AND account IN (
+            SELECT name FROM `tabAccount`
+            WHERE account_type = 'Tax' AND account_name LIKE '%%Input%%'
+            AND company = %s
+        )
+    """, (from_date, to_date, company, company), as_dict=1)
+    
+    return flt(input_vat[0].vat_amount) if input_vat and input_vat[0].vat_amount is not None else 0
+
+def get_eu_acquisitions_vat(company, from_date, to_date):
+    # Get VAT on EU acquisitions (reverse charge)
+    eu_vat = frappe.db.sql("""
+        SELECT SUM(credit) as vat_amount
+        FROM `tabGL Entry`
+        WHERE posting_date BETWEEN %s AND %s
+        AND company = %s
+        AND account IN (
+            SELECT name FROM `tabAccount`
+            WHERE account_type = 'Tax' AND account_name LIKE '%%Acquisition%%'
+            AND company = %s
+        )
+    """, (from_date, to_date, company, company), as_dict=1)
+    
+    return flt(eu_vat[0].vat_amount) if eu_vat and eu_vat[0].vat_amount is not None else 0
+
+def get_total_sales(company, from_date, to_date):
+    # Get total sales excluding VAT
+    sales = frappe.db.sql("""
+        SELECT SUM(base_net_total) as amount
+        FROM `tabSales Invoice`
+        WHERE posting_date BETWEEN %s AND %s
+        AND company = %s
+        AND docstatus = 1
+    """, (from_date, to_date, company), as_dict=1)
+    
+    return flt(sales[0].amount) if sales and sales[0].amount is not None else 0
+
+def get_total_purchases(company, from_date, to_date):
+    # Get total purchases excluding VAT
+    purchases = frappe.db.sql("""
+        SELECT SUM(base_net_total) as amount
+        FROM `tabPurchase Invoice`
+        WHERE posting_date BETWEEN %s AND %s
+        AND company = %s
+        AND docstatus = 1
+    """, (from_date, to_date, company), as_dict=1)
+    
+    return flt(purchases[0].amount) if purchases and purchases[0].amount is not None else 0
+
+def get_eu_goods_supplies(company, from_date, to_date):
+    # EU goods supplies (exports to EU)
+    eu_goods = frappe.db.sql("""
+        SELECT SUM(si.base_net_total) as amount
+        FROM `tabSales Invoice` si
+        INNER JOIN `tabSales Invoice Item` sii ON si.name = sii.parent
+        INNER JOIN `tabCustomer` c ON si.customer = c.name
+        WHERE si.posting_date BETWEEN %s AND %s
+        AND si.company = %s
+        AND si.docstatus = 1
+        AND c.territory IN (
+            SELECT name FROM `tabTerritory` 
+            WHERE parent_territory = 'European Union'
+            AND name != 'Cyprus'
+        )
+        AND sii.item_code IN (
+            SELECT name FROM `tabItem` WHERE item_group = 'Products'
+        )
+    """, (from_date, to_date, company), as_dict=1)
+    
+    return flt(eu_goods[0].amount) if eu_goods and eu_goods[0].amount is not None else 0
+
+def get_eu_services_supplies(company, from_date, to_date):
+    # EU services supplies
+    eu_services = frappe.db.sql("""
+        SELECT SUM(si.base_net_total) as amount
+        FROM `tabSales Invoice` si
+        INNER JOIN `tabSales Invoice Item` sii ON si.name = sii.parent
+        INNER JOIN `tabCustomer` c ON si.customer = c.name
+        WHERE si.posting_date BETWEEN %s AND %s
+        AND si.company = %s
+        AND si.docstatus = 1
+        AND c.territory IN (
+            SELECT name FROM `tabTerritory` 
+            WHERE parent_territory = 'European Union'
+            AND name != 'Cyprus'
+        )
+        AND sii.item_code IN (
+            SELECT name FROM `tabItem` WHERE item_group = 'Services'
+        )
+    """, (from_date, to_date, company), as_dict=1)
+    
+    return flt(eu_services[0].amount) if eu_services and eu_services[0].amount is not None else 0
+
+def get_non_eu_exports(company, from_date, to_date):
+    # Non-EU exports
+    exports = frappe.db.sql("""
+        SELECT SUM(si.base_net_total) as amount
+        FROM `tabSales Invoice` si
+        INNER JOIN `tabCustomer` c ON si.customer = c.name
+        WHERE si.posting_date BETWEEN %s AND %s
+        AND si.company = %s
+        AND si.docstatus = 1
+        AND c.territory NOT IN (
+            SELECT name FROM `tabTerritory` 
+            WHERE parent_territory = 'European Union'
+        )
+        AND c.territory != 'Cyprus'
+    """, (from_date, to_date, company), as_dict=1)
+    
+    return flt(exports[0].amount) if exports and exports[0].amount is not None else 0
+
+def get_out_of_scope_sales(company, from_date, to_date):
+    # Out of scope sales (zero-rated, exempt)
+    out_of_scope = frappe.db.sql("""
+        SELECT SUM(si.base_net_total) as amount
+        FROM `tabSales Invoice` si
+        INNER JOIN `tabSales Taxes and Charges` stc ON si.name = stc.parent
+        WHERE si.posting_date BETWEEN %s AND %s
+        AND si.company = %s
+        AND si.docstatus = 1
+        AND stc.rate = 0
+    """, (from_date, to_date, company), as_dict=1)
+    
+    return flt(out_of_scope[0].amount) if out_of_scope and out_of_scope[0].amount is not None else 0
+
+def get_eu_goods_acquisitions(company, from_date, to_date):
+    # EU goods acquisitions
+    eu_countries = ["Austria", "Belgium", "Bulgaria", "Croatia", "Czech Republic", 
+                    "Denmark", "Estonia", "Finland", "France", "Germany", 
+                    "Greece", "Hungary", "Ireland", "Italy", "Latvia", 
+                    "Lithuania", "Luxembourg", "Malta", "Netherlands", "Poland", 
+                    "Portugal", "Romania", "Slovakia", "Slovenia", "Spain", "Sweden"]
+    
+    # Convert to SQL format for IN clause
+    eu_countries_str = "','".join(eu_countries)
+    eu_countries_str = f"('{eu_countries_str}')"
+    
+    # EU goods acquisitions
+    eu_goods = frappe.db.sql(f"""
+        SELECT SUM(pi.base_net_total) as amount
+        FROM `tabPurchase Invoice` pi
+        INNER JOIN `tabPurchase Invoice Item` pii ON pi.name = pii.parent
+        INNER JOIN `tabSupplier` s ON pi.supplier = s.name
+        WHERE pi.posting_date BETWEEN %s AND %s
+        AND pi.company = %s
+        AND pi.docstatus = 1
+        AND s.country IN {eu_countries_str}
+        AND s.country != 'Cyprus'
+        AND pii.item_code IN (
+            SELECT name FROM `tabItem` WHERE item_group = 'Products'
+        )
+    """, (from_date, to_date, company), as_dict=1)
+    
+    return flt(eu_goods[0].amount) if eu_goods and eu_goods[0].amount is not None else 0
+
+def get_eu_services_acquisitions(company, from_date, to_date):
+    # EU services acquisitions
+    eu_countries = ["Austria", "Belgium", "Bulgaria", "Croatia", "Czech Republic", 
+                    "Denmark", "Estonia", "Finland", "France", "Germany", 
+                    "Greece", "Hungary", "Ireland", "Italy", "Latvia", 
+                    "Lithuania", "Luxembourg", "Malta", "Netherlands", "Poland", 
+                    "Portugal", "Romania", "Slovakia", "Slovenia", "Spain", "Sweden"]
+    
+    # Convert to SQL format for IN clause
+    eu_countries_str = "','".join(eu_countries)
+    eu_countries_str = f"('{eu_countries_str}')"
+    
+    # EU services acquisitions
+    eu_services = frappe.db.sql(f"""
+        SELECT SUM(pi.base_net_total) as amount
+        FROM `tabPurchase Invoice` pi
+        INNER JOIN `tabPurchase Invoice Item` pii ON pi.name = pii.parent
+        INNER JOIN `tabSupplier` s ON pi.supplier = s.name
+        WHERE pi.posting_date BETWEEN %s AND %s
+        AND pi.company = %s
+        AND pi.docstatus = 1
+        AND s.country IN {eu_countries_str}
+        AND s.country != 'Cyprus'
+        AND pii.item_code IN (
+            SELECT name FROM `tabItem` WHERE item_group = 'Services'
+        )
+    """, (from_date, to_date, company), as_dict=1)
+    
+    return flt(eu_services[0].amount) if eu_services and eu_services[0].amount is not None else 0
+
+@frappe.whitelist()
+def export_vat_return(filters):
+    """
+    Export VAT return in the format required by Cyprus tax authorities
+    """
+    import json
+    if isinstance(filters, str):
+        filters = json.loads(filters)
+    
+    # Get the tax ID from the company - no longer passed as a filter
+    company = filters.get('company')
+    tax_id = frappe.db.get_value("Company", company, "tax_id")
+    
+    # Ensure dates are set if using fiscal_year and quarter
+    if not filters.get('from_date') or not filters.get('to_date'):
+        fiscal_year = filters.get('fiscal_year')
+        quarter = filters.get('quarter')
+        if fiscal_year and quarter:
+            dates = get_quarter_dates(fiscal_year, quarter)
+            filters['from_date'] = dates['from_date']
+            filters['to_date'] = dates['to_date']
+    
+    # Generate the file content based on Cyprus Tax Authority requirements
+    content = "Cyprus VAT Return\n"
+    content += f"Company: {company}\n"
+    content += f"Tax ID: {tax_id}\n"
+    content += f"Period: {filters.get('from_date')} to {filters.get('to_date')}\n\n"
+    
+    # Add VAT details - actual implementation would format this according to Cyprus tax authority specs
+    data = get_data(filters)
+    for row in data:
+        content += f"{row['vat_field']}: {row['description']}: {row['amount']}\n"
+    
+    # Save the file
+    file_name = f"cyprus_vat_return_{filters.get('from_date')}_{filters.get('to_date')}.txt"
+    file_url = frappe.utils.file_manager.save_file(
+        file_name, 
+        content, 
+        "Company", 
+        company, 
+        is_private=1
+    )
+    
+    return file_url.get('file_url')
