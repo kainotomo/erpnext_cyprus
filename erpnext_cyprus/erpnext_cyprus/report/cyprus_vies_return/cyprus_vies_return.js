004ada4d1c11f797cc0720074d10e4d26d1d76e8
Panayiotis Halouvas <phalouvas@gmail.com>
2025-04-28 08:21:12 +0000

    feat: Add Cyprus VIES Return report with filters for company and date range

---
+++ erpnext_cyprus/erpnext_cyprus/report/cyprus_vies_return/cyprus_vies_return.js
@@ -0,0 +1,21 @@
+// Copyright (c) 2025, KAINOTOMO PH LTD and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Cyprus VIES Return"] = {
+	"filters": [
+		{
+            "fieldname": "company",
+            "label": __("Company"),
+            "fieldtype": "Link",
+            "options": "Company",
+            "default": frappe.defaults.get_user_default("Company"),
+            "reqd": 1
+        },
+        {
+            fieldname: "date_range",
+            label: __("Date Range"),
+            fieldtype: "DateRange",
+            reqd: 1
+        }
+	]
+};
