a7fa281af0d66822b7cadf8f31721f43bce5c7f5
Panayiotis Halouvas <phalouvas@gmail.com>
2025-05-12 05:44:58 +0000

    feat: Implement access token retrieval and subscription update for Bank Of Cyprus integration

--- erpnext_cyprus/erpnext_cyprus/doctype/bank_of_cyprus/bank_of_cyprus.py
+++ erpnext_cyprus/erpnext_cyprus/doctype/bank_of_cyprus/bank_of_cyprus.py
@@ -49,6 +49,27 @@ class BankOfCyprus(Document):
 		
 		return self.create_subscription()
 	
+	def get_access_token_2(self):
+
+		url = self.get_base_url() + "/oauth2/token"
+		payload = {
+			"grant_type": "authorization_code",
+			"scope": "UserOAuth2Security",
+			"client_id": self.client_id,
+			"client_secret": self.get_password("client_secret"),
+			"code": self.code
+		}
+		headers = {
+			"Accept": "application/json",
+			"Content-Type": "application/x-www-form-urlencoded"
+		}
+		response = requests.post(url, data=payload, headers=headers)
+		if (response.status_code != 200):
+			return response.json()
+		frappe.db.set_value('Bank Of Cyprus', self.name, 'access_token_2', response.text)
+		
+		return self.update_subscription()
+	
 	def create_subscription(self):
 
 		access_token_1 = json.loads(self.access_token_1)
@@ -79,8 +100,7 @@ class BankOfCyprus(Document):
 			"Content-Type": "application/json",
 			"Authorization": "Bearer " + access_token_1["access_token"],
 			"timeStamp": datetime.utcnow().isoformat(),
-			"journeyId": str(uuid.uuid4()),
-			"app_name": "ERPNext Integration"
+			"journeyId": str(uuid.uuid4())
 		}
 		response = requests.post(url, json=payload, headers=headers)
 		if (response.status_code != 200 and response.status_code != 201):
@@ -88,15 +108,60 @@ class BankOfCyprus(Document):
 		frappe.db.set_value('Bank Of Cyprus', self.name, 'subscription_id', response.text)
 		return response.json()
 	
+	def update_subscription(self):
+
+		subscription_id = json.loads(self.subscription_id)
+		access_token_2 = json.loads(self.access_token_2)
+		url = self.get_base_url() + "/v1/subscriptions/" + subscription_id["subscriptionId"]
+		
+		# First get the subscription to get accounts
+		payload = {}
+		headers = {
+			"Accept": "application/json",
+			"Content-Type": "application/json",
+			"Authorization": "Bearer " + access_token_2["access_token"],
+			"originUserId": self.user_id,
+			"timeStamp": datetime.utcnow().isoformat(),
+			"journeyId": str(uuid.uuid4()),
+			"app_name": "ERPNext Integration"
+		}
+		response = requests.get(url, params=payload, headers=headers)
+		if (response.status_code != 200 and response.status_code != 201):
+			frappe.throw("Something went wrong with Bank Of Cyprus authorization")
+		frappe.db.set_value('Bank Of Cyprus', self.name, 'subscription_id', response.text)
+		response_json = response.json()[0]
+
+		# Then patch to activate subscription
+		payload = {
+			"selectedAccounts": response_json["selectedAccounts"],
+			"accounts": response_json["accounts"],
+			"payments": response_json["payments"],
+			"customerInformation": response_json["customerInformation"]
+		}
+		headers = {
+			"Accept": "application/json",
+			"Content-Type": "application/json",
+			"Authorization": "Bearer " + access_token_2["access_token"],
+			"originUserId": self.user_id,
+			"timeStamp": datetime.utcnow().isoformat(),
+			"journeyId": str(uuid.uuid4()),
+			"app_name": "ERPNext Integration"
+		}
+		response = requests.patch(url=url, json=payload, headers=headers)
+		if (response.status_code != 200 and response.status_code != 201):
+			frappe.throw("Something went wrong with Bank Of Cyprus authorization")
+		frappe.db.set_value('Bank Of Cyprus', self.name, 'subscription_id', response.text)
+		return response.json()
+
 	@frappe.whitelist()
 	def initiate_web_application_flow(self):
 		"""Return an authorization URL. Save state in Token Cache."""
 
 		subscription_id = self.get_access_token_1()['subscriptionId']
-
+		
 		authorization_url = self.get_base_url() + "/oauth2/authorize"
 		state = self.base64_encode(frappe.generate_hash(length=10))
-		frappe.db.set_value('Hellenic Bank', self.name, 'state', state)
+		frappe.db.set_value('Bank Of Cyprus', self.name, 'state', state)
 		query_params = {
 			"response_type": "code",
 			"redirect_uri": self.redirect_uri,
@@ -107,75 +172,6 @@ class BankOfCyprus(Document):
 		}
 		authorization_url += "?" + urlencode(query_params)
 		return authorization_url
-
-@frappe.whitelist()
-def get_access_token_2():
-	bank_of_cyprus = frappe.get_doc("Bank Of Cyprus")
-
-	url = get_base_url(bank_of_cyprus) + "/oauth2/token"
-	payload = {
-		"grant_type": "authorization_code",
-		"scope": "UserOAuth2Security",
-		"client_id": bank_of_cyprus.client_id,
-		"client_secret": bank_of_cyprus.get_password("client_secret"),
-		"code": bank_of_cyprus.code
-	}
-	headers = {
-		"Accept": "application/json",
-		"Content-Type": "application/x-www-form-urlencoded"
-	}
-	response = requests.post(url, data=payload, headers=headers)
-	if (response.status_code != 200):
-		return response.json()
-	frappe.db.set_value('Bank Of Cyprus', bank_of_cyprus.name, 'access_token_2', response.text)
-	
-	return update_subscription()
-
-def update_subscription():
-	bank_of_cyprus = frappe.get_doc("Bank Of Cyprus")
-
-	subscription_id = json.loads(bank_of_cyprus.subscription_id)
-	access_token_2 = json.loads(bank_of_cyprus.access_token_2)
-	url = get_base_url(bank_of_cyprus) + "/v1/subscriptions/" + subscription_id["subscriptionId"]
-	
-	# First get the subscription to get accounts
-	payload = {}
-	headers = {
-		"Accept": "application/json",
-		"Content-Type": "application/json",
-		"Authorization": "Bearer " + access_token_2["access_token"],
-		"originUserId": bank_of_cyprus.user_id,
-		"timeStamp": datetime.utcnow().isoformat(),
-		"journeyId": str(uuid.uuid4()),
-		"app_name": "ERPNext Integration"
-	}
-	response = requests.get(url, params=payload, headers=headers)
-	if (response.status_code != 200 and response.status_code != 201):
-		frappe.throw("Something went wrong with Bank Of Cyprus authorization")
-	frappe.db.set_value('Bank Of Cyprus', bank_of_cyprus.name, 'subscription_id', response.text)
-	response_json = response.json()[0]
-
-	# Then patch to activate subscription
-	payload = {
-		"selectedAccounts": response_json["selectedAccounts"],
-		"accounts": response_json["accounts"],
-		"payments": response_json["payments"],
-		"customerInformation": response_json["customerInformation"]
-	}
-	headers = {
-		"Accept": "application/json",
-		"Content-Type": "application/json",
-		"Authorization": "Bearer " + access_token_2["access_token"],
-		"originUserId": bank_of_cyprus.user_id,
-		"timeStamp": datetime.utcnow().isoformat(),
-		"journeyId": str(uuid.uuid4()),
-		"app_name": "ERPNext Integration"
-	}
-	response = requests.patch(url=url, json=payload, headers=headers)
-	if (response.status_code != 200 and response.status_code != 201):
-		frappe.throw("Something went wrong with Bank Of Cyprus authorization")
-	frappe.db.set_value('Bank Of Cyprus', bank_of_cyprus.name, 'subscription_id', response.text)
-	return response.json()
 
 @frappe.whitelist()
 def create_accounts():
